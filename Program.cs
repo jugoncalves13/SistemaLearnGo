b534c2bbf1a027e5b5dcf6cae36b0945dc84d4b7
jugoncalves13 <julianieugeniosilva@gmail.com>
2024-10-02 12:14:28 -0300

    alteração

--- Program.cs
+++ Program.cs
@@ -10,6 +10,10 @@ builder.Services.AddDbContext<Contexto> //Juliani
     (options => options.UseSqlServer("Data Source=SP-1491034\\SQLSENAI;Initial Catalog = SistemaLearnGoNovo;" +
     " Integrated Security = True;TrustServerCertificate = True"));
 
+/*builder.Services.AddDbContext<Contexto> //Gabi
+    (options => options.UseSqlServer("Data Source=SP-1491009\\SQLSENAI;Initial Catalog = SistemaLearnGoNovo;" +
+    " Integrated Security = True;TrustServerCertificate = True"));*/
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
